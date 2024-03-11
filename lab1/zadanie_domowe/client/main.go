b6e6e2613a62f36405f367a9e7c708af9b86fda2
Rafał Laskowski <rafal.laskowski02@gmail.com>
2024-03-11 02:13:51 +0100

    again small changes

--- lab1/zadanie_domowe/client/main.go
+++ lab1/zadanie_domowe/client/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"main/client_test"
+	"main/client_back"
 )
 
 func main() {
-	client := client_test.NewClient("client1", ":8080")
+	client := client_back.NewClient("client1", ":8080")
 	err := client.Start()
 	if err != nil {
 		return
