af3ff9487df266a58f3be47eb27c77d5c4db8548
Rafał Laskowski <rafal.laskowski02@gmail.com>
2024-03-17 21:33:46 +0100

    minor changes

--- lab2/zadanie_domowe/main.go
+++ lab2/zadanie_domowe/main.go
@@ -14,19 +14,25 @@ func currencyValues(r *gin.Engine) {
 		amount := c.PostForm("amount")
 		currency1 := c.PostForm("curr1")
 		currency2 := c.PostForm("curr2")
+		statusCode := 200
+		success := true
 
 		if amount == "" && currency1 == "" && currency2 == "" {
 			fmt.Println("Error: amount, currency1 and currency2 is required")
+			success = false
+			statusCode = 400
 			return
 		}
-		success := true
+
 		converter := NewCurrencyConverter(currency1, currency2, amount)
+
 		if converter.bestPrice == 0 && converter.averagePrice == 0 && converter.finalValue == 0 {
 			fmt.Println("Wrong input!!!")
 			success = false
+			statusCode = 400
 		}
 
-		c.HTML(200, "index.html", gin.H{
+		c.HTML(statusCode, "index.html", gin.H{
 			"BestPrice":    fmt.Sprintf("%.8f", converter.bestPrice),
 			"WorstPrice":   fmt.Sprintf("%.8f", converter.worstPrice),
 			"AveragePrice": fmt.Sprintf("%.8f", converter.averagePrice),
